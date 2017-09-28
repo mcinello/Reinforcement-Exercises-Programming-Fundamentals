b99aa04ab43d1a4974ef094b9bfba00ceee93f42
Michelle Cinello <m.cinello@gmail.com>
2017-09-28 12:11:21 -0400

    started shopping-cart reinforcement assignment

---
+++ shopping-cart/something.rb
@@ -0,0 +1,18 @@
+require './stuff.rb'
+
+#reps the specific product
+
+class Something
+
+  def initialize(name, base_price, tax_rate)
+    @name = name
+    @base_price = base_price
+    @tax_rate = tax_rate / 100
+  end
+
+  def shop
+
+
+  end
+
+end
---
+++ shopping-cart/stuff.rb
@@ -0,0 +1,14 @@
+require './something.rb'
+
+#reps the shopping cart
+
+class Stuff
+
+  @@shopping_cart = []
+
+  @@total_price = 0
+
+
+  #calculates tax
+
+end
