4b9306c53847be4ca544724be70f780762eb5149
mcinello <m.cinello@gmail.com>
2017-09-28 16:33:25 -0400

    Add files via upload

--- shopping-cart/something.rb
+++ shopping-cart/something.rb
@@ -1,18 +1,36 @@
-require './stuff.rb'
-
 #reps the specific product
-
 class Something
 
-  def initialize(name, base_price, tax_rate)
+  def initialize(name, base_price)
     @name = name
     @base_price = base_price
-    @tax_rate = tax_rate / 100
+    @tax_rate = 0.13
+    @total_price = 0.0
   end
 
-  def shop
-
-
+  def total_price
+    @total_price = @base_price + (@base_price * @tax_rate)
+    return @total_price
   end
+
+#READERS
+  def base_price
+    @base_price
+  end
+
+  def tax_rate
+    @tax_rate
+  end
+
+#WRITERS
+  # def base_price(base_price)
+  #   @base_price = base_price
+  #   return @base_price
+  # end
+  #
+  # def tax_rate(tax_rate)
+  #   @tax_rate = tax_rate
+  #   return @base_price
+  # end
 
 end
