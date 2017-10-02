6932092b75bc29181fece18a2d3eed37630dc9c9
Michelle Cinello <m.cinello@gmail.com>
2017-10-02 10:51:03 -0400

    added a TodoList class and added the task instances to the list.

--- task.rb
+++ task.rb
@@ -27,8 +27,39 @@ class Task
 
 end
 
+
+class TodoList < Task
+
+    @@tasks = []
+
+
+
+  def self.add_task(task)
+    @@tasks << task
+  end
+
+  def self.tasks_list
+    @@tasks
+  end
+
+
+end
+
+
+
+
+
+
+
+
+
 task1 = Task.new("Work on assignments", "Monday, October 2, 2017")
-task2 = Task.new("Drink coffee", "Monday, October 2, 2017")
+task2 = Task.new("Drink coffee", "Monday, October 3, 2017")
+task3 = Task.new("Must adult", "Monday, October 5, 2017")
+
+TodoList.add_task(task1)
+TodoList.add_task(task2)
+TodoList.add_task(task2)
 
 
 puts task1.description
@@ -36,3 +67,8 @@ puts task1.due_date
 
 puts task2.description
 puts task2.due_date
+
+puts task3.description
+puts task3.due_date
+
+puts TodoList.tasks_list.inspect
