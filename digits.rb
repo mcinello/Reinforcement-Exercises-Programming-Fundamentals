a1627c5a8a05e7ad30aa32f62088b16acc751cfd
Michelle Cinello <m.cinello@gmail.com>
2017-10-10 19:47:45 -0400

    reusable method to translate digits

--- digits.rb
+++ digits.rb
@@ -1,6 +1,8 @@
-digits = ['1','2','3','4','5','6','7','8','9']
+$digits = ['1','2','3','4','5','6','7','8','9']
 en = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 fr = ['un','deux','trois','quatre','cinq','six','sept','huit','neuf']
+kr = ['hana','dul', 'set', 'net', 'dausut', 'yeosut', 'ilgup', 'yeodul', 'ah-hope']
+it = ['uno', 'due', 'tre', 'quattro', 'cinque', 'sei', 'sette', 'otto', 'nove']
 
 =begin
 digits must become a hash
@@ -12,14 +14,22 @@ turn all of them into hashes
 they can be accessed by INDEX
 =end
 
-digits_hash = {}
+$digits_hash = {}
 
-# def translate_digits(lang, arr)
-  digits.each_with_index do |index, value|
-    digits_hash[index.to_i] = {english: en[value]}
+def translate_digits(lang, arr)
+  $digits.each_with_index do |val, idx|
+    hash = $digits_hash[val.to_i]
+    if hash == nil
+      hash = {}
+    end
+    hash[lang] = arr[idx]
+    $digits_hash[val.to_i] = hash
   end
-# end
+end
 
-# digits_hash.translate_digits("english", en)
+translate_digits("english", en)
+translate_digits("french", fr)
+translate_digits("korean", kr)
+translate_digits("italian", it)
 
-puts digits_hash
+puts $digits_hash.inspect
