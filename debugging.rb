f9b3ff38f05a5b6c0550d470b8655b7bb2a280d4
Michelle Cinello <m.cinello@gmail.com>
2017-10-13 12:39:22 -0400

    user can't pick up more than 3 cards

--- debugging.rb
+++ debugging.rb
@@ -1,10 +1,18 @@
 def select_cards(possible_cards, hand)
 
+  counter = 0
+
   possible_cards.each do |current_card|
     print "Do you want to pick up #{current_card}?"
     answer = gets.chomp
     if answer.downcase == 'y'
       hand << current_card
+      counter += 1
+    end
+    if counter > 3
+      puts "can't pick up any more cards"
+      counter = 0
+      return hand
     end
   end
     return hand
