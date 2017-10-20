32bd4ad3dff968734aaf9e142847f4782443d5dc
Michelle Cinello <m.cinello@gmail.com>
2017-10-20 16:21:20 -0400

    Tama is in chosen seat

--- seating-chart.rb
+++ seating-chart.rb
@@ -18,6 +18,16 @@ seating_chart.each_with_index do |val, row|
   val.each_with_index do |num, seat|
     if num == nil
       puts "Row #{row + 1} seat #{seat + 1} is free"
+      puts "Do you want to sit there?"
+      user_input = gets.chomp
+        if user_input == 'y'
+          puts "What is your name?"
+          user_name = gets.chomp
+          val << user_name
+          puts "#{user_name} is at row #{row + 1} seat #{seat + 1}"
+        end
     end
   end
 end
+
+puts seating_chart.inspect
