a86c5a1875749f9aa46cea942513ca85e6be3aba
Michelle Cinello <m.cinello@gmail.com>
2017-10-26 12:10:06 -0400

    fixed indentation

--- long-lowercase.rb
+++ long-lowercase.rb
@@ -2,7 +2,7 @@ words = ["Totam", "ut", "odit","quis", "Maiores", "unde", "EX", "EST", "corporis
 
 words.each do |word|
   if word == word.downcase && word.length > 4
-  puts "long and lowercase"
+    puts "long and lowercase"
   elsif word == word.downcase
     puts "lowercase"
   elsif word.length > 4
